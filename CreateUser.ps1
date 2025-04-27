eb86277f2a6a83d4e5d2244fd5821813f3364441
Said Hussein <123634164+cyb4x@users.noreply.github.com>
2025-04-28 02:29:06 +0300

    Update CreateUser.ps1

--- CreateUser.ps1
+++ CreateUser.ps1
@@ -1,18 +1,32 @@
 param (
     [string]$username,
-    [string]$password = "P@ssw0rd"
+    [string]$password = "P@ssw0rd",
+    [switch]$Delete
 )
 
-try {
-    net user $username $password /add | Out-Null
-    net localgroup administrators $username /add | Out-Null
-    net localgroup "Remote Desktop Users" $username /add | Out-Null
-    net group "Domain Admins" $username /add /domain 2>&1 | Out-Null
-    Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name fDenyTSConnections -Value 0 | Out-Null
-    Enable-NetFirewallRule -DisplayGroup "Remote Desktop" | Out-Null
-    netsh advfirewall firewall set rule group="Remote Desktop" new enable=yes | Out-Null
-    Write-Host "Account created for $username with password $password"
-}
-catch {
-    Write-Host "An error occurred: $_" -ForegroundColor Red
+if ($Delete) {
+    try {
+        net user $username /delete | Out-Null
+        net localgroup administrators $username /delete | Out-Null
+        net localgroup "Remote Desktop Users" $username /delete | Out-Null
+        net group "Domain Admins" $username /delete /domain 2>&1 | Out-Null
+        Write-Host "Account $username deleted and all changes reverted."
+    }
+    catch {
+        Write-Host "An error occurred: $_" -ForegroundColor Red
+    }
+} else {
+    try {
+        net user $username $password /add | Out-Null
+        net localgroup administrators $username /add | Out-Null
+        net localgroup "Remote Desktop Users" $username /add | Out-Null
+        net group "Domain Admins" $username /add /domain 2>&1 | Out-Null
+        Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name fDenyTSConnections -Value 0 | Out-Null
+        Enable-NetFirewallRule -DisplayGroup "Remote Desktop" | Out-Null
+        netsh advfirewall firewall set rule group="Remote Desktop" new enable=yes | Out-Null
+        Write-Host "Account created for $username with password $password"
+    }
+    catch {
+        Write-Host "An error occurred: $_" -ForegroundColor Red
+    }
 }
